84c424f55ccd68ef0f2d8226fb6fdfe850df76f6
Nikita <DeadPool38rus@yandex.ru>
2017-04-07 19:03:16 +0700

    Добавлен минимальный функционал

---
+++ Инженерка/Инженерка/Инженерка.cpp
@@ -0,0 +1,14 @@
+// Инженерка.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world" << endl;
+    return 0;
+}
+
